a61b1992e0968f01ee5d559854d58e2915d3c15f
end2043 <41049394+end2043@users.noreply.github.com>
2022-01-02 10:14:17 +0900

    commit

--- src/TestClass.java
+++ src/TestClass.java
@@ -2,8 +2,8 @@
 public class TestClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	System.out.println("Hello! This is Github Practicing!");
+	//Github Practice!
 	}
 
 }
